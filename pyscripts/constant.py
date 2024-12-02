b206fbb92921ece0df671f4b59c7bd81ad07d118
ms4118 <cityu.ms4118@gmail.com>
2024-12-02 21:44:48 +0800

    Update constant.py

--- pyscripts/constant.py
+++ pyscripts/constant.py
@@ -2,17 +2,14 @@ import math
 
 def order(D, S, C, I, LT):
     try:
-        # Calculate total cost and economic order quantity 
         numerator = 2 * D * S
         denominator = I * C
         Q_squared = numerator / denominator
         Q = math.sqrt(Q_squared)
         
-        # Round up
         Q_up = math.ceil(Q)
         TC = (D / Q_up) * S + (I * C / 2) * Q_up
 
-        # Calculate Reorder point quantity in units 
         R_square = (D / 52) * LT
         R_up = math.ceil(R_square)
 
@@ -26,25 +23,20 @@ def order(D, S, C, I, LT):
 
 def noninstant(D, S, C, I, LT, P):
     try:
-        
-        # Calculate total cost and economic order quantity 
         numerator = 2 * D * S
         denominator = I * C
         Q_squared = numerator / denominator
         Q = math.sqrt(Q_squared)
         
-        # Round up
         Q_up = math.ceil(Q)
         TC = (D / Q_up) * S + (I * C / 2) * Q_up
-
-        # Calculate Reorder point quantity in units 
+ 
         R_square = (D / 52) * LT
         R_up = math.ceil(R_square)
 
         Q_square= Q_up * math.sqrt(P/(P-D/52))
         round_Q = math.ceil(Q_square)
-        
-        # Conclusion 
+         
         print(f"When the inventory level drops to {R_up} units, place a replenishment order for {round_Q}.")
         return f"When the inventory level drops to {R_up}</br>units, place a replenishment order for {round_Q}."
         
