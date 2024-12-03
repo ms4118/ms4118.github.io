8d34e6e862c139ac3160f4569f7c080ea8cbd1c8
ms4118 <cityu.ms4118@gmail.com>
2024-12-03 13:50:58 +0800

    Update constant.py

--- pyscripts/constant.py
+++ pyscripts/constant.py
@@ -2,21 +2,20 @@ import math
 
 def order(D, S, C, I, LT):
     try:
-        # Calculate total cost and economic order quantity 
+        # Calculate eoq
         numerator = 2 * D * S
         denominator = I * C
         Q_squared = numerator / denominator
         Q = math.sqrt(Q_squared)
-        
-        # Round up
+
         Q_up = math.ceil(Q)
         TC = (D / Q_up) * S + (I * C / 2) * Q_up
 
-        # Calculate Reorder point quantity in units 
+
         R_square = (D / 52) * LT
         R_up = math.ceil(R_square)
 
-        # Conclusion 
+        #Out put
         print(f"When the inventory level drops to {R_up} units, place a replenishment order for {Q_up}.")
         return f"When the inventory level drops to {R_up}</br>units, place a replenishment order for {Q_up}."
         
@@ -27,13 +26,13 @@ def order(D, S, C, I, LT):
 def noninstant(D, S, C, I, LT, P):
     try:
         
-        # Calculate total cost and economic order quantity 
+        # eoq here
         numerator = 2 * D * S
         denominator = I * C
         Q_squared = numerator / denominator
         Q = math.sqrt(Q_squared)
         
-        # Round up
+
         Q_up = math.ceil(Q)
         TC = (D / Q_up) * S + (I * C / 2) * Q_up
 
@@ -44,7 +43,7 @@ def noninstant(D, S, C, I, LT, P):
         Q_square= Q_up * math.sqrt(P/(P-D/52))
         round_Q = math.ceil(Q_square)
         
-        # Conclusion 
+        # output 
         print(f"When the inventory level drops to {R_up} units, place a replenishment order for {round_Q}.")
         return f"When the inventory level drops to {R_up}</br>units, place a replenishment order for {round_Q}."
         
